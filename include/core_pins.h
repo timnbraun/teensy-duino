b8c3c5b20044c17198976158fc2465dc41ab712f
Tim Braun <tim@tim-braun.com>
2023-01-19 10:29:55 -0600

    Per upstream: Add Time functions, Add delayNanoseconds for Teensy 3.x & LC

--- include/core_pins.h
+++ include/core_pins.h
@@ -2424,9 +2424,103 @@ static inline void delayMicroseconds(uint32_t usec)
 }
 #endif
 
-
-
-
+static inline void delayNanoseconds(uint32_t) __attribute__((always_inline, unused));
+static inline void delayNanoseconds(uint32_t nsec)
+{
+	if (__builtin_constant_p(nsec)) {
+		// use NOPs for the common usage of a constexpr input and short delay
+		if (nsec == 0) return;
+		if (nsec <= 1000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop");
+			return;
+		}
+		if (nsec <= 2000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop");
+			return;
+		}
+		if (nsec <= 3000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop");
+			return;
+		}
+		if (nsec <= 4000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop");
+			return;
+		}
+		if (nsec <= 5000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			return;
+		}
+		if (nsec <= 6000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop");
+			return;
+		}
+		if (nsec <= 7000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop");
+			return;
+		}
+		if (nsec <= 8000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop");
+			return;
+		}
+		if (nsec <= 9000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop\n nop");
+			return;
+		}
+		if (nsec <= 10000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			return;
+		}
+		if (nsec <= 11000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop");
+			return;
+		}
+		if (nsec <= 12000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop");
+			return;
+		}
+		if (nsec <= 13000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop");
+			return;
+		}
+		if (nsec <= 14000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop\n nop");
+			return;
+		}
+		if (nsec <= 15000 / (F_CPU / 1000000)) {
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			__asm__ volatile("nop\n nop\n nop\n nop\n nop");
+			return;
+		}
+	}
+	uint32_t n = nsec * (F_CPU / 45776) >> 16;
+	if (n == 0) return;
+	__asm__ volatile(
+		"L_%=_delayNanoseconds:"		"\n\t"
+#ifdef KINETISL
+		"sub    %0, #1"				"\n\t"
+		"bne    L_%=_delayNanoseconds"		"\n"
+		: "+l" (n) :
+#else
+		"subs   %0, #1"				"\n\t"
+		"bne    L_%=_delayNanoseconds"		"\n"
+		: "+r" (n) :
+#endif
+	);
+}
 
 
 
@@ -2439,6 +2533,20 @@ void rtc_set(unsigned long t);
 void rtc_compensate(int adjust);
 #ifdef __cplusplus
 }
+
+// DateTimeFields follows C library "struct tm" convention, but uses much less memory
+typedef struct  {
+	uint8_t sec;   // 0-59
+	uint8_t min;   // 0-59
+	uint8_t hour;  // 0-23
+	uint8_t wday;  // 0-6, 0=sunday
+	uint8_t mday;  // 1-31
+	uint8_t mon;   // 0-11
+	uint8_t year;  // 70-206, 70=1970, 206=2106
+} DateTimeFields;
+void breakTime(uint32_t time, DateTimeFields &tm);  // break 32 bit time into DateTimeFields
+uint32_t makeTime(const DateTimeFields &tm); // convert DateTimeFields to 32 bit time
+
 class teensy3_clock_class
 {
 public:
